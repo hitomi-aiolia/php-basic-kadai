5364ab2eb3bb12006b56a8efedfa2dce5b5657b6
nagahama hitomi <godof1thewind@gmail.com>
2025-10-26 20:04:20 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+<body>
+<?php
+ $food_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+print_r($food_data);
+
+?>
+</body>
