41bce85c7695ba42bbaa2fdc5d86ae94b7d7e1d3
nagahama hitomi <godof1thewind@gmail.com>
2025-11-04 21:09:56 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta cherset="utf-8">
+    <title>PHP基礎編</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      // ソートする配列を宣言
+      $nums = [15, 4, 18, 23, 10 ];
+
+      function sort_2way($array, $order) {
+        if($order) {
+          sort( $array);
+          echo '昇順にソートします。<br>';
+          foreach ($array as $array) {
+          echo $array . '<br>';
+          }
+        } else {
+          rsort( $array);
+          echo '降順にソートします。<br>';
+          foreach ($array as $array) {
+            echo $array . '<br>';
+          }
+        }
+      }
+      
+      sort_2way($nums, TRUE);
+      sort_2way($nums, FALSE);
+      ?>
+    </p>
+  </body>
+</html>
